2b239080069f653b3443cd9cfefa13da465c455d
Aykut Alpgiray Ates <alpgirayaykut@gmail.com>
2018-12-16 12:45:47 +0300

    typescript definition files (#3)
    * type deffinations added

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+import { History } from "history";
+import { Action, Middleware, Reducer, Store } from "redux";
+
+declare function createReduxHistory(store: Store): History;
+
+export interface IHistoryContextOptions {
+  history: History;
+  routerReducerKey?: string;
+  oldLocationChangePayload?: boolean;
+  reduxTravelling?: boolean;
+}
+
+export interface IHistoryContext {
+  createReduxHistory: typeof createReduxHistory;
+  routerMiddleware: Middleware;
+  routerReducer: Reducer;
+}
+
+export function createReduxHistoryContext(
+  options: IHistoryContextOptions
+): IHistoryContext;
+export const push: (to: string) => Action;
