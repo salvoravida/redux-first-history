44920aba67d05245ac3890f99bb703aa98e4d727
salvoravida <salvoravida@gmail.com>
2021-08-16 14:40:54 +0200

    feat add History Router for rr6

---
+++ rr6/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Router } from 'react-router';
+
+export function HistoryRouter({ children, history }) {
+   const [state, setState] = React.useState({
+      action: history.action,
+      location: history.location,
+   });
+   React.useLayoutEffect(() => history.listen(setState), [history]);
+   return React.createElement(Router, Object.assign({ children, navigator: history }, state));
+}
