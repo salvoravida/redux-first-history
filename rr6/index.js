9a02d2e5d542bc1864facdd75a7df7ea66281798
Salvatore Ravidà <salvoravida@gmail.com>
2021-12-09 14:45:49 +0100

    fix: rr6 es5 build for jest support

--- rr6/index.js
+++ rr6/index.js
@@ -1,18 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+   return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HistoryRouter = void 0;
 /* eslint-disable react/no-children-prop */
-import React from 'react';
-import { Router } from 'react-router';
-
-export function HistoryRouter({ basename, children, history }) {
-   const [state, setState] = React.useState({
+var react_1 = __importDefault(require("react"));
+var react_router_1 = require("react-router");
+function HistoryRouter(_a) {
+   var basename = _a.basename, children = _a.children, history = _a.history;
+   var _b = react_1.default.useState({
       action: history.action,
       location: history.location,
-   });
-   React.useLayoutEffect(() => history.listen(setState), [history]);
-   return React.createElement(Router, {
-      basename,
-      children,
+   }), state = _b[0], setState = _b[1];
+   react_1.default.useLayoutEffect(function () { return history.listen(setState); }, [history]);
+   return react_1.default.createElement(react_router_1.Router, {
+      basename: basename,
+      children: children,
       location: state.location,
       navigationType: state.action,
       navigator: history,
    });
 }
+exports.HistoryRouter = HistoryRouter;
